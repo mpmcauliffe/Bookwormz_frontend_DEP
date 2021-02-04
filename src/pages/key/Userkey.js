1b3bb7a59f168572ab5d958bdfa00841bd84f5a7
mpmcauliffe <mcauliffe99@gmail.com>
2021-02-04 03:21:48 -0700

    create basic lock

--- src/pages/key/Userkey.js
+++ src/pages/key/Userkey.js
@@ -1,12 +1,45 @@
 import React from 'react'
+import styled from 'styled-components'
+import './spinner.css'
 
+// see https://loading.io/css/ for cool CSS spinners
+const SpinnerContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: .5;
 
+    .lds-dual-ring {
+        display: inline-block;
+        width: 80px;
+        height: 80px;
+    }
+    .lds-dual-ring:after {
+        content: " ";
+        display: block;
+        width: 100px;
+        height: 100px;
+        margin: 8px;
+        border-radius: 50%;
+        border: 15px solid #6b1f39;
+        border-color: #6b1f39 transparent #6b1f39 transparent;
+        animation: lds-dual-ring 3s linear infinite;
+    }
+    @keyframes lds-dual-ring {
+        0% {
+        transform: rotate(0deg);
+        }
+        100% {
+        transform: rotate(360deg);
+        }
+    }
+`
 export const Userkey = props => {
-    console.log(document.cookie)
-
     return (
-        <div>
-            <h1>User Key</h1>            
-        </div>
+        <SpinnerContainer>
+            <div className='lds-dual-ring'></div>            
+        </SpinnerContainer>
     )
 }
